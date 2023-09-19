3394b165d87cb81fb6c4c7c4ff21d4156b164dde
05rhuy <huypq1@apero.vn>
2023-09-19 16:04:40 +0700

    create icon

---
+++ src/components/Icons/ArrowRight.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function ArrowRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={25}
+      height={24}
+      viewBox="0 0 25 24"
+      fill="none"
+    >
+      <path
+        d="M5.5 12H19.5"
+        stroke="white"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.5 5L19.5 12L12.5 19"
+        stroke="white"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
