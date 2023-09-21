5eb3e92341526d0a71b369eac10a3892302a53ad
trung <trungld@apero.vn>
2023-09-21 17:59:34 +0700

    fix and build

--- src/pages/GenerateAvatar/components/Modals/ModalPayment.tsx
+++ src/pages/GenerateAvatar/components/Modals/ModalPayment.tsx
@@ -70,7 +70,7 @@ export default function ModalPayment({
   const handleClickPurchase = () => {
     const payload: any = {
       priceId: price.id,
-      redirectUrl: `${window.location.protocol}/${window.location.host}` + ROUTES.APP_PAGE,
+      redirectUrl: `${window.location.protocol}//${window.location.host}` + ROUTES.APP_PAGE,
       // redirectUrl: 'https://avatar.apero.vn/',
     };
     if (isLoggedIn) {
