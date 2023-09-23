41d1b08b725d34e2960d1705a7476dbc6e99ce88
05rhuy <huypq1@apero.vn>
2023-09-23 14:59:59 +0700

    refactor sale page code

--- src/components/SalePage/Payment/index.tsx
+++ src/components/SalePage/Payment/index.tsx
@@ -34,40 +34,18 @@ import generateService from '@/services/generate.service';
 interface PropsType {
   handleSelectPrice: (price: any) => void;
   priceSelected: any;
+  prices: any[];
 }
 
 export default function Payment({
   priceSelected,
   handleSelectPrice,
+  prices,
 }: PropsType) {
   // const prices = useAppSelector((state: RootState) => state.app.prices);
-  const [prices, setPrices] = useState<any[]>([]);
+
   const { handlePurchase } = usePurchase();
   const tolerance = discountPrice > 0 ? 0.01 : 0;
-
-  const priceType =
-    discountPrice === 0.5
-      ? 'sale50'
-      : discountPrice === 0.25
-      ? 'sale25'
-      : 'main';
-
-  useQuery(
-    ['get-list-price'],
-    () => generateService.getListPrice({ type: priceType }),
-    {
-      onSuccess: (res: any) => {
-        const listPrice = res.data?.map((item: any) => ({
-          id: item.id,
-          name: item.metadata.name,
-          price: item.unit_amount / 100,
-          maxStyle: Number(item.metadata.numberStyle),
-          bestOffer: item.metadata?.popular === 'true',
-        }));
-        setPrices(listPrice);
-      },
-    }
-  );
 
   useEffect(() => {
     if (prices.length > 1) {
